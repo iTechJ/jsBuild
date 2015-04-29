3c1369a526bcc700b8e9c4417f539323c4f5c830
AntonNekrasov <Anton.Nekrasov@wsa-212.icx.local>
2015-04-29 14:44:58 +0300

    step5

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var uglify = require('gulp-uglify'); //npm install gulp-uglify --save-dev
 var rename = require('gulp-rename'); //npm install gulp-rename --save-dev
 var minifycss = require('gulp-minify-css'); //npm install gulp-minify-css --save-dev
 var notify = require('gulp-notify'); //npm install gulp-notify  --save-dev
+var livereload = require('gulp-livereload'); //npm install gulp-livereload  --save-dev
+var del = require('del'); //npm install del --save-dev
 
 gulp.task('script', function() {
     return gulp.src([
@@ -36,8 +38,29 @@ gulp.task('style', function() {
         .pipe(notify({ message: 'Styles task complete' }));
 });
 
+gulp.task('clean', function(cb) {
+    del(['dist/'], cb)
+});
+
+gulp.task('default', ['clean'], function() {
+    //gulp.start();
+    gulp.start('style', 'script');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+    gulp.watch('style/*.css', ['style']);
+    gulp.watch('js/**/*.js', ['script']);
+    livereload.listen();
+    // Watch any files in dist/, reload on change
+    //gulp.watch(['dist/**']).on('change', livereload.changed);
+
+});
+
 //Step 0: install gulp
 //Step 1: add jshint
 //Step 2: concatenate js
 //Step 3: minify & uglify js
 //Step 4: minify css
+//Step 5: live reload
